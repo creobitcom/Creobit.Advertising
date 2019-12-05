fb911f25c2e9ea62b9fee381b75fb75c92c971c3
Igor Fatnev <razkrimaka@gmail.com>
2019-12-06 01:00:31 +0300

    Интерфейс и реализация менеджера инициализации

---
+++ Core/Scripts/IPlatformAuthManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IPlatformAuthManager
+{
+    IEnumerable<string> AuthenticatedPlatforms { get; }
+    bool IsInitialized { get; }
+
+    void Initialize(Action onInitialize);
+}
---
+++ Core/Scripts/PlatformAuthManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public sealed class PlatformAuthManager : IPlatformAuthManager
+{
+    #region IPlatformAuthManager
+
+    public IEnumerable<string> AuthenticatedPlatforms
+    {
+        get
+        {
+            var result = default(IEnumerable<string>);
+
+            if (IsInitialized)
+            {
+                result = _platforms.
+                    Where(platform => platform.IsInitialized).
+                    Select(platform => platform.Id);
+            }
+            return result;
+        }
+    }
+
+    public bool IsInitialized => _waitingInitializations == 0;
+
+    public void Initialize(Action onInitialize)
+    {
+        _waitingInitializations = _platforms.Count();
+
+        foreach (var platform in _platforms)
+        {
+            platform.Initialize(
+                () => OnInitializedResult(platform.Id, true), 
+                () => OnInitializedResult(platform.Id, false));
+        }
+
+        void OnInitializedResult(string id, bool result)
+        {
+            _waitingInitializations--;
+            Debug.Log($"Инициализация {id}; Результат {result}");
+
+            if (_waitingInitializations == 0)
+            {
+                onInitialize?.Invoke();
+            }
+        }
+    }
+
+    #endregion
+
+    public PlatformAuthManager(IEnumerable<IPlatformAuth> platforms)
+    {
+        _platforms = platforms;
+    }
+
+    private IEnumerable<IPlatformAuth> _platforms;
+    private int _waitingInitializations = -1;
+}
